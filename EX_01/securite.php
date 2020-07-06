f9c1ae9bf9fd065896e5141562e1ab7c34ac2b5d
Alexandra-frieh <67266090+Alexandra-frieh@users.noreply.github.com>
2020-07-06 17:29:58 +0200

    travail jour 9

---
+++ EX_01/securite.php
@@ -0,0 +1,16 @@
+<?php
+
+ if($_POST['password']==2020){ session_start();
+     
+     $_SESSION['id'] = $_POST['login'];
+     header ("Location: http://localhost:8888/ISCC%202020/ISCC-2020-J09/EX_01/mini-site-routing.php?page=1");
+    
+     setcookie("id" , $_SESSION['id'], time()+3600);
+ }
+ else{
+     echo "<p> Mauvais couple identifiant/mot de passe</p>";
+     echo "<p> Cliquez <a href='http://localhost:8888/ISCC%202020/ISCC-2020-J09/EX_01/mini-site-routing.php?page=connexion'>ici</a> pour retourner sur la page connexion";
+ }
+
+
+    ?>
